59d8c62d68e6a68030a2007ee9810536a7da9c89
eskopp <skopp.erik@gmail.com>
2023-12-12 14:25:58 +0100

    del bash

--- change_title.sh
+++
@@ -1,6 +0,0 @@
-for file in *.html; do
-    title=$(grep -oP '<title>\K[^<]+' "$file")
-    if [ -n "$title" ]; then
-        sed -i "46s|<h1>.*</h1>|<h1>$title</h1>|" "$file"
-    fi
-done
--- copy_name.sh
+++
@@ -1,3 +0,0 @@
-for file in *.html; do
-  sed -i "s|<h1>Generic</h1>|<h1>$(basename -s .html "$file")</h1>|" "$file"
-done
